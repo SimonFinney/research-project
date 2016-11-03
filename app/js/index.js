d2e5fce252447a06621768148404ce3a1e6ab9ec
Simon Finney <simonfin@ie.ibm.com>
2016-11-03 22:35:06 +0000

    Adding logic to toggle image open and closed

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,47 @@
 // Main
 
+// TODO: Comments
+let app;
+let detailedImage;
+let imageLinks;
+let isImageToggled;
+
+
+function toggleImage() {
+  if (isImageToggled) {
+    app.removeAttribute('data-fixed');
+    detailedImage.removeAttribute('data-active');
+  } else {
+    app.setAttribute('data-fixed', '');
+    detailedImage.setAttribute('data-active', '');
+  }
+
+  isImageToggled = !isImageToggled;
+}
+
+
+function setDetailedImage(event) {
+  event.preventDefault();
+
+  const imageToToggle = event.target;
+
+  detailedImage.alt = imageToToggle.getAttribute('alt');
+  detailedImage.src = imageToToggle.getAttribute('src');
+
+  toggleImage();
+}
+
+
 function init() {
-  console.log('init');
+  app = document.querySelector('[data-app]');
+  detailedImage = app.querySelector('.img');
+  imageLinks = app.querySelectorAll('.images__a');
+
+  detailedImage.addEventListener('click', toggleImage);
+
+  imageLinks.forEach(imageLink =>
+    imageLink.addEventListener('click', setDetailedImage)
+  );
 }
 
 document.addEventListener('DOMContentLoaded', init);
