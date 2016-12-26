4cc644c93ea7ff8018b8671a865d6d7f50f9c006
Simon Finney <simonfin@ie.ibm.com>
2016-12-26 22:32:23 +0100

    Adding CRUD operations, create listeners, and count.

--- src/database.js
+++ src/database.js
@@ -1,4 +1,6 @@
 // Database
+
+// TODO: Comments
 const firebase = require('firebase');
 
 const firebaseConfiguration =
@@ -7,4 +9,67 @@ const firebaseConfiguration =
 const firebaseApp = firebase.initializeApp(firebaseConfiguration);
 const database = firebaseApp.database();
 
-module.exports = { database };
+const ref = 'data';
+const data = database.ref(ref);
+
+
+function count(callback) {
+  data.once('value')
+    .then(value =>
+      callback(value.numChildren())
+    );
+}
+
+
+function create(newData, callback) {
+  data.push(newData)
+    .then(callback);
+}
+
+
+function getById(id) {
+  return data.child(id);
+}
+
+
+function del(id) {
+  getById(id).remove();
+}
+
+
+function get(callback) {
+  data.once('value')
+    .then(value =>
+      callback(value.val())
+    );
+}
+
+
+function init() {
+  data.on('child_added', value => console.log(value.key));
+}
+
+
+function read(id, callback) {
+  getById(id).once('value')
+    .then(value =>
+      callback(value.val())
+    );
+}
+
+
+function update(id, newData) {
+  getById(id).set(newData);
+}
+
+
+init();
+
+module.exports = {
+  count,
+  create,
+  del,
+  get,
+  read,
+  update,
+};
